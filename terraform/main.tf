4107051d8a598dee17dc972629cbf0bf88ede9d1
Sophia Hudson <sophia@1strategy.com>
2017-08-24 14:05:22 -0600

    add s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,11 @@ provider "aws" {
   }
   region = "${var.project_region}"
 }
+
+resource "aws_s3_bucket" "pipeline-artifacts-bucket" {
+  bucket = "${var.project_name}-pipeline-artifacts-bucket"
+  acl = "private"
+  tags {
+    Name = "${var.project_name}-pipeline-artifacts-bucket"
+  }
+}
